1700986865ad43250494e4a022f44db77ea11150
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-09 14:32:55 +0100

    Meglio

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
 $(document).ready(function() {
 
-    var numbers = [1,2,3,4,5];
-    alert(numbers);
+    numbers = nRandMinToMax(5,1,500);
+    alert('ricorda: ' + numbers);
     
     var start;
     function startFun() {
-        start = setTimeout(userPrompt(numbers),3000);
+        start = setTimeout(userPrompt,3000,numbers);
     };
     startFun();
 
 });
 
-function userPrompt(arr) {
-    var howMany = 5;
+function userPrompt(array) {
+    var howMany = array.length;
     var user = [];
     var ok = [];
     for (var i = 0; i < howMany; i++) {
         user[i] = prompt('inserire numero');
-        var is = whereIsInArray(user[i],arr);
+        var is = whereIsInArray(user[i],array);
         if (is != -1) {
             ok.push(user[i]);
         }
     }
     
     console.log('hai indovinato ' + ok.length + ' numeri: ' + ok);
-
-}
+};
 
 function whereIsInArray(value,array) {
     var index = -1;
@@ -40,4 +39,24 @@ function whereIsInArray(value,array) {
         }
     }
     return index;
-}
+};
+
+function nRandMinToMax(n,min,max) {
+
+    var array = [];
+
+    if (max - min + 1 >= n) {
+        for (var i = 0; i < n; i++) {
+            array[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+            while (array.indexOf(array[i]) != -1 && array.indexOf(array[i]) < i) { /* per assicurarsi che siano tutti diversi */
+                array[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+        }
+    }
+    else if (max - min + 1 < n) {
+        alert('impossibile generare numeri diversi tra loro');
+    }
+
+    return array;
+
+};
