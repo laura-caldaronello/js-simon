202eb53e7e94c35c63a2bb95108f7cc998f7f166
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-11 16:57:27 +0100

    Qualche correzione

--- bonus/main3.js
+++ bonus/main3.js
@@ -30,20 +30,20 @@ $(document).ready(function() {
     $('body').append('<h2 id=\'time2\'></h2>');
 
     document.getElementById('input-time').innerHTML = '';
-
+    
     document.getElementById('time1').innerHTML = 'Secondi trascorsi: ';
     document.getElementById('time2').innerHTML = 'Livello: ';
-
+    
     var c = 30;
     var t = 0;
     var q = 0;
     var seconds = setInterval(function() {
-
+        
         document.getElementById('input-time').innerHTML = 'Hai ancora ' + c + ' secondi per scrivere';
 
         document.getElementById('time1').innerHTML = 'Secondi trascorsi: ' + t;
         document.getElementById('time2').innerHTML = 'Livello: ' + (q + 1);
-        
+
         if (c == 30) {
             $('#input-time').hide();
             document.getElementById('text').innerHTML = 'ricorda: ' + currentNumbersArray[q];
@@ -58,37 +58,37 @@ $(document).ready(function() {
             document.getElementById('input-time').style.color = 'red';
         }
         else if (c == 0) {
-
+            
             if (q == 4) {
-                alert('Hai vinto!')
+                alert('Hai vinto!');
                 clearInterval(seconds);
             }
-
+            
             if ($('#text input').val() != String(currentNumbersArray[q])) {
-                alert('Hai perso!')
+                alert('Hai perso!');
                 clearInterval(seconds);
             }
-
+            
             c = 30;
             q++;
-
+            
             $('#input-time').hide();
             document.getElementById('text').innerHTML = 'ricorda: ' + currentNumbersArray[q];
             
         }
-
+        
         c--;
         t++;
-                
+        
     },1000);
-            
-
+    
 });
 
+
 function nRandMinToMax(n,min,max) {
-
+    
     var array = [];
-
+    
     if (max - min + 1 >= n) {
         for (var i = 0; i < n; i++) {
             array[i] = Math.floor(Math.random() * (max - min + 1)) + min;
